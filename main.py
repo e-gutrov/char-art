4b7cc08ddb1863bfb3bf59eed2117eb088434ce6
e.gutrov <gutrov.egor@gmail.com>
2019-10-27 21:15:29 +0300

    Sorted chars.json, fixed to_wb func

--- main.py
+++ main.py
@@ -3,26 +3,22 @@ import numpy as np
 import json
 
 
-chars = []
-
-
-def init():
-    global chars
+def init_chars():
     with open('chars.json', 'r') as fin:
         chars = json.load(fin)
+    return chars
 
 
 def to_wb(a):
-    return round((int(a[0])+a[1]+a[2]) / 3)
+    return round(sum(a) / len(a))
 
 
-def get_char(x):
-    global chars
-    return chars[int(91 / 256 * x)]
+def get_char(x, chars):
+    return chars[int(len(chars) / 256 * x)][0]
 
 
 def scale(a, need_size):
-    need_size = (need_size[0], round(need_size[1] * 1.5))
+    need_size = (need_size[0], round(need_size[1] * 1.5))  # chars' width is less than height
     scale_i = a.shape[0] / need_size[0]
     scale_j = a.shape[1] / need_size[1]
     b = np.zeros(need_size).astype(np.uint8)
@@ -33,35 +29,34 @@ def scale(a, need_size):
 
 
 def main():
-    init()
-    name = input('Введите имя файла: ')
-    # name = 'test128.png'
+    chars = init_chars()
+    name = input('Enter a filename: ')
     img = Image.open(name)
     rgb_cols = np.asarray(img)
     wb_cols = np.zeros((rgb_cols.shape[0], rgb_cols.shape[1])).astype(np.uint8)
-
+    print('Wait a bit...')
     for i in range(wb_cols.shape[0]):
         for j in range(wb_cols.shape[1]):
             wb_cols[i][j] = to_wb(rgb_cols[i][j][:3])
 
-    tp = input(
-        'Размер изобрежения {0}x{1} пикселей. Уменьшить размер в n раз-1, задать свой размер-2.'.format(
+    operation = input(
+        'Image size is {0}x{1} pts.\n'
+        'Enter "dec x" to decrease size by x times\n'
+        'Enter "man w h" to get image with size w h (in chars): '.format(
             wb_cols.shape[0], wb_cols.shape[1]
         )
     )
-    if tp == '1':
-        n = float(input('Во сколько раз вы хотите уменьшить размер?'))
-        sz = tuple((int(wb_cols.shape[0] / n), int(wb_cols.shape[1] / n)))
+    if operation[:3] == 'dec':
+        times = float(operation.split()[1])
+        sz = tuple((int(wb_cols.shape[0] / times), int(wb_cols.shape[1] / times)))
     else:
-        sz = tuple(map(int, input(
-            'Введите итоговый размер картинки (в символах). '
-            'Рекомендуется не более {0}x{1} и не менее {2}x{3}: '.format(1, 2, 3, 4)).split()))
+        sz = tuple(map(int, operation.split()[2:0:-1]))
 
-    with open('output.txt', 'w') as f:
+    with open('result.txt', 'w') as f:
         char_picture = scale(wb_cols, sz)
         for i in range(char_picture.shape[0]):
             for j in range(char_picture.shape[1]):
-                print(get_char(char_picture[i][j]), end='', file=f)
+                print(get_char(char_picture[i][j], chars), end='', file=f)
             print(file=f)
 
 
